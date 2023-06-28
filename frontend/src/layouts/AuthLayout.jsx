81a468d90727914de44f4813e77c863a0fa78f80
Ivan Gonzalez <hal308366@gmail.com>
2023-06-28 13:51:00 -0700

    feat(frontend): create to new password pages

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <>
-      <p>AuthLayout</p>
-
-      <Outlet />
-    </>
+    <main className="container mx-auto mt-5 md:mt-20 md:flex md:justify-center">
+      <div className="md:w-2/3 lg:w-2/5">
+        <Outlet />
+      </div>
+    </main>
   );
 }
--- frontend/src/pages/NewPassword.jsx
+++ frontend/src/pages/NewPassword.jsx
@@ -1,9 +1,109 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import Alerta from "../components/Alert";
+import clienteAxios from "../../config/clienteAxios";
 
 export default function NewPassword() {
+  const [password, setPassword] = useState("");
+  const [tokenValido, setTokenValido] = useState(false);
+  const [alerta, setAlerta] = useState({});
+  const [passwordModificado, setPasswordModificado] = useState(false);
+
+  const params = useParams();
+  const { token } = params;
+
+  useEffect(() => {
+    const buyerToken = async () => {
+      try {
+        // TODO: Mover hacia un cliente axios
+        await clienteAxios.get(`/users/forgot-password/${token}`);
+        setTokenValido(true);
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+    };
+    buyerToken();
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password.length < 6) {
+      setAlerta({
+        msg: "El Password debe ser minimo de 6 caracteres",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      const url = `/users/forgot-password/${token}`;
+
+      const { data } = await clienteAxios.post(url, { password });
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+      setPasswordModificado(true);
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
-    <div>
-      <p>NewPassword</p>
-    </div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Reset your password and dont lose access to your
+        <span className="text-slate-700">projects</span>
+      </h1>
+
+      {msg && <Alerta alerta={alerta} />}
+
+      {tokenValido && (
+        <form
+          className="my-10 bg-white shadow rounded-lg p-10"
+          onSubmit={handleSubmit}
+        >
+          <div className="my-5">
+            <label
+              className="uppercase text-gray-600 block text-xl font-bold"
+              htmlFor="password"
+            >
+              New Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Escribe tu Nuevo Password"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <input
+            type="submit"
+            value="Guardar Nuevo Password"
+            className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+          />
+        </form>
+      )}
+
+      {passwordModificado && (
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/"
+        >
+          Log in
+        </Link>
+      )}
+    </>
   );
 }
