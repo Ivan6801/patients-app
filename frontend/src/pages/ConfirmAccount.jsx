8d22c69a0aaba7eced32689fdf68f35b92300150
Ivan Gonzalez <hal308366@gmail.com>
2023-06-28 10:33:38 -0700

    feat(frontend): Confirming the account via Token

--- frontend/src/pages/ConfirmAccount.jsx
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alerta from "../components/Alert";
+import clienteAxios from "../../config/clienteAxios";
 
 export default function ConfirmAccount() {
+  const [alerta, setAlerta] = useState({});
+  const [confirmedAccount, setConfirmedAccount] = useState(false);
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    const confirmBill = async () => {
+      try {
+        const url = `/users/confirm/${id}`;
+        const { data } = await clienteAxios.get(url);
+        setAlerta({
+          msg: data.msg,
+          error: false,
+        });
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+    };
+    confirmBill();
+  }, []);
+
+  const { msg } = alerta;
+
   return (
-    <div>
-      <p>ConfirmAccount</p>
-    </div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Confirm your account start creating your
+        <span className="text-slate-700">Project</span>
+      </h1>
+
+      <div>
+        {msg && <Alerta alerta={alerta} />}
+        {confirmedAccount && (
+          <Link
+            className="block text-center my-5 text-slate-500 uppercase text-sm"
+            to="/"
+          >
+            Log in
+          </Link>
+        )}
+      </div>
+    </>
   );
 }
--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,9 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Alerta from "../components/Alert";
+import clienteAxios from "../../config/clienteAxios";
 
 export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email === "" || email.length < 6) {
+      setAlerta({
+        msg: "El Email es obligatorio",
+        error: true,
+      });
+      return;
+    }
+    try {
+      const { data } = await clienteAxios.post(`/users/forgot-password/`, {
+        email,
+      });
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+  const { msg } = alerta;
   return (
-    <div>
-      <p>ForgotPassword</p>
-    </div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Forget password and manage your{" "}
+        <span className="text-slate-700">projects</span>
+      </h1>
+      {msg && <Alerta alerta={alerta} />}
+      <form
+        onSubmit={handleSubmit}
+        className="my-10 bg-white shadow rounded-lg p-10 py-5"
+      >
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Registration Email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Send instructions"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/"
+        >
+          You do not have an account? Log in
+        </Link>
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/register"
+        >
+          You do not have an account? Sign up
+        </Link>
+      </nav>
+    </>
   );
 }
